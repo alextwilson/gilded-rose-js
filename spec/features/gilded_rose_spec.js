1566c27ea86b2d33ba7576a3c22b3da7a6c18fd0
alextwilson <github@alextwilson.co.uk>
2018-02-27 17:42:53 +0000

    Tests added for normal items
    Tests that for positive sellIn quality and sellIn reduce by one
    Tests that when sellin is 0 quality reduces by two
    Tests that quality does not degrade passed 0

--- spec/features/gilded_rose_spec.js
+++ spec/features/gilded_rose_spec.js
@@ -1,7 +1,54 @@
 describe('Gilded Rose', function () {
-  it('should foo', function () {
-    const gildedRose = new Shop([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual('foo');
+  describe('updateQuality', function () {
+    describe('normal items', function () {
+      describe('positive sellIn values', function () {
+        beforeEach(function() {
+          const gildedRose = new Shop([new Item('normalItem', 5, 10)]);
+          this.items = gildedRose.updateQuality();
+        });
+
+        it('should still have the same name', function () {
+          expect(this.items[0].name).toEqual('normalItem');
+        });
+
+        it('sellIn should reduce by one', function () {
+          expect(this.items[0].sellIn).toEqual(4);
+        });
+
+        it('quality should reduce by one', function () {
+          expect(this.items[0].quality).toEqual(9);
+        });
+      });
+
+      describe('sellIn is 0', function () {
+        beforeEach(function() {
+          const gildedRose = new Shop([new Item('normalItem', 0, 10)]);
+          this.items = gildedRose.updateQuality();
+        });
+
+        it('sellIn should reduce by one', function () {
+          expect(this.items[0].sellIn).toEqual(-1);
+        });
+
+        it('quality should reduce by two', function () {
+          expect(this.items[0].quality).toEqual(8);
+        });
+      });
+
+      describe('quality is 0', function () {
+        beforeEach(function() {
+          const gildedRose = new Shop([new Item('normalItem', 0, 0)]);
+          this.items = gildedRose.updateQuality();
+        });
+
+        it('sellIn should reduce by one', function () {
+          expect(this.items[0].sellIn).toEqual(-1);
+        });
+
+        it('quality should not change', function () {
+          expect(this.items[0].quality).toEqual(0);
+        });
+      });
+    });
   });
 });
