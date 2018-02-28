89c4134122b65051b6bcb0f1ef6b6bb7344af2a4
alextwilson <github@alextwilson.co.uk>
2018-02-28 14:10:34 +0000

    Normal item updates correctly in all circumastances

--- spec/units/normal_item_spec.js
+++ spec/units/normal_item_spec.js
@@ -20,18 +20,49 @@ describe('Normal Item', function () {
   });
 
   describe('updateItem', function () {
-    beforeEach(function() {
-      item = new NormalItem('vest', 10, 5);
+    describe('positive sellIn values', function () {
+      beforeEach(function() {
+        item = new NormalItem('vest', 10, 5);
+        item.updateItem();
+      });
+
+      it('decreases sellIn by one', function () {
+        expect(item.sellIn).toEqual(9)
+      });
+
+      it('decreases quality by one', function () {
+        expect(item.quality).toEqual(4)
+      });
     });
 
-    it('decreases sellIn by one', function () {
-      item.updateItem();
-      expect(item.sellIn).toEqual(9)
+    describe('sellIn is 0', function () {
+      beforeEach(function() {
+        item = new NormalItem('vest', 0, 5);
+        item.updateItem();
+      });
+
+      it('decreases sellIn by one', function () {
+        expect(item.sellIn).toEqual(-1)
+      });
+
+      it('decreases quality by two', function () {
+        expect(item.quality).toEqual(3)
+      });
     });
 
-    it('decreases quality by one', function () {
-      item.updateItem();
-      expect(item.quality).toEqual(4)
+    describe('quality is 0', function () {
+      beforeEach(function() {
+        item = new NormalItem('vest', 0, 0);
+        item.updateItem();
+      });
+
+      it('sellIn should reduce by one', function () {
+        expect(item.sellIn).toEqual(-1);
+      });
+
+      it('quality should not change', function () {
+        expect(item.quality).toEqual(0);
+      });
     });
   });
 });
--- src/normal_item.js
+++ src/normal_item.js
@@ -1,6 +1,16 @@
 class NormalItem extends Item {
   updateItem() {
     this.sellIn -= 1;
-    this.quality -= 1;
+    this._updateQuality();
+  }
+
+  _updateQuality() {
+    if (this.quality === 0) return;
+
+    if (this.sellIn < 1) {
+      this.quality -= 2;
+    } else {
+      this.quality -= 1;
+    }
   }
 }
