628a5fa9d25e129093724f1f7f93d3a8f8c7ab96
alextwilson <github@alextwilson.co.uk>
2018-06-01 23:25:35 +0100

    Adds LegendaryItem to feature test

--- spec/features/gilded_rose_spec.js
+++ spec/features/gilded_rose_spec.js
@@ -8,7 +8,7 @@ describe('Gilded Rose', function () {
         normalItem = new NormalItem('Vest', 10, 5),
         timedItem = new TimedItem('Backstage passes to a TAFKAL80ETC concert', 20, 10),
         agedItem = new AgedItem('Aged Brie', 10, 5),
-        new LegendaryItem('Sulfuras, Hand of Ragnaros', 5, 80),
+        legendaryItem = new LegendaryItem('Sulfuras, Hand of Ragnaros', 5, 80),
         new ConjuredItem('Conjured Shoes', 10, 5)
       ];
       shop = new Shop(items);
@@ -19,6 +19,7 @@ describe('Gilded Rose', function () {
       expect(normalItem.sellIn).toEqual(9);
       expect(timedItem.sellIn).toEqual(19);
       expect(agedItem.sellIn).toEqual(9);
+      expect(legendaryItem.sellIn).toEqual(5);
     });
 
     it('changes quality value of all items appropriately', function () {
@@ -26,6 +27,7 @@ describe('Gilded Rose', function () {
       expect(normalItem.quality).toEqual(4);
       expect(timedItem.quality).toEqual(11);
       expect(agedItem.quality).toEqual(6);
+      expect(legendaryItem.quality).toEqual(80);
     });
   });
 });
